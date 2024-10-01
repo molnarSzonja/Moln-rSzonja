1c3b74674d729ca9b731c570b4ad602892f101ca
molnarSzonja <szonja307@gmail.com>
2024-10-01 20:12:40 +0200

    IntroducePage kész

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let scrollContainer = document.querySelector('.gallery');
 
-  
+function autoScroll() {
+  scrollContainer.scrollLeft += 1;
+  if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+    scrollContainer.scrollLeft = 0; 
+  }
+}
 
+
+let autoScrollInterval = setInterval(autoScroll, 20); 
